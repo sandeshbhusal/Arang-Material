8b297f036c01df93ac4cfe41f8ea3258e20d6a8d
Sandesh Bhusal <sandeshbhusal@outlook.com>
2015-12-08 18:28:42 +0545

    driver.html :)

--- js/driver.js
+++ js/driver.js
@@ -1,17 +1,21 @@
+var jammed = false;
+var userLat = 0;
+var userLng = 0;
+$("#updt").click(function(){
+    jammed = $("#jamstat").is(":checked");
+    alert(jammed);
+    navigator.geolocation.getCurrentPosition(setPos);
+    function setPos(Data){
+        userLat = Data.coords.latitude;
+        userLng = Data.coords.longitude;
+    }
+});
 if(Parse){
-    app.netInfo();
+    //app.netInfo();
+    var vId = app.user.vehicleID;
     var vehicles = Parse.Object.extend("vehicle");
     var vehicle = new vhicles();
     var query = new Parse.Query();
-    query.get("ID", {
-        success:function(data){
-            var name = data.get("Jammed");
-            var name = data.get("ServiceOf");
-        },
-        error:function(object, errorNo){
-            
-        }
-    });
 }
 else{
     app.netInfo();
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     if(!app.user.name){
-        $("#wrapper").load("setup.html");
+        $("#wrapper").load("driver.html");
     }
     else{
         if(!app.user.runTime){
@@ -16,7 +16,7 @@ $(document).ready(function(){
             }
         }
     }
-    Parse.initialize("uzexoP2RQGMSARWVpi9VZsmp1Po7lGFHjzTsaXSZ", "K6qkNFSDvoE0h5biL7mmsPXuQwc083ESkdYZt1E7");
+    //Parse.initialize("uzexoP2RQGMSARWVpi9VZsmp1Po7lGFHjzTsaXSZ", "K6qkNFSDvoE0h5biL7mmsPXuQwc083ESkdYZt1E7");
     if(!Parse){
         app.netInfo();
     }
