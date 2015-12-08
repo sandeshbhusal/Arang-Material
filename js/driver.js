418719f154ab9b5cf0545cd476bdc38dac241432
Sandesh Bhusal <sandeshbhusal@outlook.com>
2015-12-08 18:16:38 +0545

    wtf

--- js/driver.js
+++ js/driver.js
@@ -1,3 +1,18 @@
 if(Parse){
-    
+    app.netInfo();
+    var vehicles = Parse.Object.extend("vehicle");
+    var vehicle = new vhicles();
+    var query = new Parse.Query();
+    query.get("ID", {
+        success:function(data){
+            var name = data.get("Jammed");
+            var name = data.get("ServiceOf");
+        },
+        error:function(object, errorNo){
+            
+        }
+    });
+}
+else{
+    app.netInfo();
 }
