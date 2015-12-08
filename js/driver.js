401f1d0642761ed2623fed9c73e8b6a7ea6e3969
Sandesh Bhusal <sandeshbhusal@outlook.com>
2015-12-08 18:59:52 +0545

    uff.

--- js/driver.js
+++ js/driver.js
@@ -9,14 +9,22 @@ $("#updt").click(function(){
         userLat = Data.coords.latitude;
         userLng = Data.coords.longitude;
     }
+    if(Parse){
+        //app.netInfo();
+        var vId = app.user.vehicleID;
+        var vehicles = Parse.Object.extend("vehicle");
+        var vehicle = new vhicles();
+        var query = new Parse.Query();
+        query.set(vId, {
+            success: function(){
+                
+            },
+            error: function(){
+                
+            }
+        });
+    }
+    else{
+        app.netInfo();
+    }
 });
-if(Parse){
-    //app.netInfo();
-    var vId = app.user.vehicleID;
-    var vehicles = Parse.Object.extend("vehicle");
-    var vehicle = new vhicles();
-    var query = new Parse.Query();
-}
-else{
-    app.netInfo();
-}
