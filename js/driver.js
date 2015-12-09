3ecae6bc843d991938edd945ec657fdade829576
Sandesh Bhusal <sandeshbhusal@outlook.com>
2015-12-09 06:52:50 +0545

    Driver.js Updated
    Driver.js full support added. :) to completion!!!!

--- js/driver.js
+++ js/driver.js
@@ -3,28 +3,44 @@ var userLat = 0;
 var userLng = 0;
 $("#updt").click(function(){
     jammed = $("#jamstat").is(":checked");
-    alert(jammed);
     navigator.geolocation.getCurrentPosition(setPos);
     function setPos(Data){
         userLat = Data.coords.latitude;
         userLng = Data.coords.longitude;
     }
-    if(Parse){
-        //app.netInfo();
-        var vId = app.user.vehicleID;
-        var vehicles = Parse.Object.extend("vehicle");
-        var vehicle = new vhicles();
-        var query = new Parse.Query();
-        query.set(vId, {
-            success: function(){
-                
-            },
-            error: function(){
-                
-            }
-        });
-    }
-    else{
-        app.netInfo();
-    }
+if(Parse){
+    //app.netInfo();
+    var vId = app.user.vehicleID;
+    var vehicles = Parse.Object.extend("vehicle");
+    var vehicle = new vehicles();
+    var query = new Parse.Query(vehicles);
+    query.get("tBmGsy4Z9O", {
+        success:function(data){
+            var olLon = data.get("LonNew");
+            var olLat = data.get("LatNew");
+            
+            console.log("Successfully retreieved object");
+            data.save(null, {
+                success:function(object){
+                    object.set("LatNew", userLat);
+                    object.set("LonNew", userLng);
+                    object.set("LatOld", olLat);
+                    object.set("LonOld", olLon);
+                    object.set("Jammed", $("#jamstat").is(":checked"));
+                    object.save();
+                    location.reload();
+                },
+                error:function(){
+                    console.log("Could not update the data! :(");
+                }
+            });
+        },
+        error:function(object, errorID){
+            console.log("Could not retrieve object");
+        }
+    });
+}
+else{
+    app.netInfo();
+}
 });
